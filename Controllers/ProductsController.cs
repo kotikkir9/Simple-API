8dbbbecc9c7fe6b4c36de1b44a3789f39a7d6b54
Kirill Zhukau <67194208+kotikkir9@users.noreply.github.com>
2022-03-19 02:16:14 +0100

    Removed unused Usings

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -4,8 +4,6 @@ using SimpleAPI.Models.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleAPI.Controllers
 {
